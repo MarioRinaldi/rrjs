01b8a1fef49aa9a5a38b32fb7b797b46fd522775
Renato Rodrigues <rerodrigues@sp.r7.com>
2010-11-17 13:02:32 -0200

    Ligeiramente lintificado...

--- rr.js
+++ rr.js
@@ -78,18 +78,18 @@ var rr = {
         // Define variaveis condicionais
         vars.msg          = (vars.title)     ? ("<h1>" + vars.title + "</h1>" + msg):(msg);
         vars.className    = (vars.auxClass)  ? (vars.className + " " + vars.auxClass):(vars.className);
-        vars.cancelButton = (vars.buttons  && (vars.buttons == 'yesNo' || vars.buttons == 'okCancel'));
+        vars.cancelButton = (vars.buttons  && (vars.buttons === 'yesNo' || vars.buttons === 'okCancel'));
         
         if (vars.buttons) {
-            rr.str['button1'] = (vars.buttons == 'yesNo')?(rr.str['noButton']):(rr.str['cancelButton']);
-            rr.str['button2'] = (vars.buttons == 'yesNo')?(rr.str['yesButton']):(rr.str['okButton']);
+            rr.str['button1'] = (vars.buttons === 'yesNo')?(rr.str['noButton']):(rr.str['cancelButton']);
+            rr.str['button2'] = (vars.buttons === 'yesNo')?(rr.str['yesButton']):(rr.str['okButton']);
         } else {
             rr.str['button1'] = rr.str['cancelButton'];
             rr.str['button2'] = rr.str['okButton'];
         }
         
-        if (vars.width) vars.marginLeft = (((vars.width  / 2) * - 1) + "px");
-        if (vars.height) vars.marginTop = (((vars.height / 2) * - 1) + "px");
+        if (vars.width) { vars.marginLeft = (((vars.width  / 2) * - 1) + "px"); }
+        if (vars.height) { vars.marginTop = (((vars.height / 2) * - 1) + "px"); }
         
         if (rr.getElement('rrPopupMessage')) { popupMessage_hide.call(); }
 
@@ -135,11 +135,11 @@ var rr = {
         // Anexa elementos ao documento
         mainDiv.appendChild(textDiv);
         mainDiv.appendChild(buttonDiv);
-        if (vars.cancelButton) buttonDiv.appendChild(cancelButton);
+        if (vars.cancelButton) { buttonDiv.appendChild(cancelButton); }
         buttonDiv.appendChild(okButton);
         document.body.appendChild(mainDiv);
         
-        if (vars.overlay) rr.createOverlay(okButton, vars.overlay);
+        if (vars.overlay) { rr.createOverlay(okButton, vars.overlay); }
         okButton.focus();
         return false;
     },
@@ -149,8 +149,8 @@ var rr = {
         
         // Define variaveis condicionais
         vars.className  = (vars.auxClass)  ? (vars.className + " " + vars.auxClass):(vars.className);
-        if (vars.width) vars.marginLeft = (((vars.width  / 2) * - 1) + "px");
-        if (vars.height) vars.marginTop = (((vars.height / 2) * - 1) + "px");
+        if (vars.width) { vars.marginLeft = (((vars.width  / 2) * - 1) + "px"); }
+        if (vars.height) { vars.marginTop = (((vars.height / 2) * - 1) + "px"); }
         
         if (rr.getElement('rrPopupFrame')) { rr.popupFrame_hide.call(); }
         
@@ -158,7 +158,7 @@ var rr = {
         mainDiv.id = "rrPopupFrame";
         mainDiv.className = vars.className;
         
-        if (vars.closeButton == 'yes') {
+        if (vars.closeButton === 'yes') {
             var xDiv     = document.createElement("div");
             xDiv.id = "xDiv";
             xDiv.className = "x";
@@ -171,22 +171,20 @@ var rr = {
         contentDiv.className = "frameContent";
         mainDiv.appendChild(contentDiv);
         
-        if (vars.type == "iframe") {    
+        if (vars.type === "iframe") {    
             contentDiv.allowTransparency = "true";
             contentDiv.scrolling = "auto";
             contentDiv.frameBorder = "0";
             contentDiv.src = src;
-        } else if (vars.type == "div") {
+        } else if (vars.type === "div") {
 			if (vars.content) {
 				var place = document.createElement('div');
 				contentDiv.appendChild(place);
 				contentDiv.replaceChild(vars.content, place);
-
-
-
+                
 			} else {
-            rr.ajax.replaceContent (contentDiv, src);
-        	}
+                rr.ajax.replaceContent (contentDiv, src);
+            }
         }
         // Define estilos condicionais
         if (vars.width && vars.marginLeft) {
@@ -198,9 +196,9 @@ var rr = {
              mainDiv.style.marginTop = vars.marginTop; }
 
         document.body.appendChild(mainDiv);
-        if (vars.closeButton == 'yes') rr.addEvent("xImg","click",rr.popupFrame_hide);
-        if (vars.overlay) rr.createOverlay(rr.getElement('xImg'), vars.overlay);
-		if (vars.callback) vars.callback.call();
+        if (vars.closeButton === 'yes') { rr.addEvent("xImg","click",rr.popupFrame_hide); }
+        if (vars.overlay) { rr.createOverlay(rr.getElement('xImg'), vars.overlay); }
+		if (vars.callback) { vars.callback.call(); }
         return false;
     },
     
@@ -227,13 +225,13 @@ var rr = {
              overlay.style.filter = "alpha(opacity=" + vars.opacity + ")";
              overlay.style.opacity = (vars.opacity / 100); }
              
-        if (vars.color) overlay.style.backgroundColor = vars.color;
+        if (vars.color) { overlay.style.backgroundColor = vars.color; }
         
         document.body.appendChild(overlay);
 		//IE 6 Only - Hacks & Bug fixes
-		if ( window.ActiveXObject && typeof document.body.style.maxHeight == "undefined" ) {
+		if ( window.ActiveXObject && typeof document.body.style.maxHeight === "undefined" ) {
 			var ifrm = document.createElement("iframe");
-			ifrm.id = "rrIframe"
+			ifrm.id = "rrIframe";
 			ifrm.src = "javascript:'<html></html>';";
 	        ifrm.scrolling = "auto";
 			ifrm.frameBorder = "0";
@@ -246,7 +244,7 @@ var rr = {
 			overlayHeight = (contentHeight < windowHeight) ? windowHeight : contentHeight;
 			overlay.style.height = overlayHeight + "px";
 		}
-        if (!elem && vars.modal != "yes") elem = overlay;
+        if (!elem && vars.modal !== "yes") { elem = overlay; }
         rr.addEvent(elem, "click", rr.overlay_hide);
         return false;
     },
@@ -257,8 +255,8 @@ var rr = {
 	onlyNumbers: function (evt, v) {
         var vars = rr.getVars(v);
 		
-		var k = evt.keyCode
-		if (!(k == 46 ||k == 8 ||k == 9 || k >= 48 && k <= 57 || k >= 96 && k <= 105 || k >= 35 && k <= 39)) {
+		var k = evt.keyCode;
+		if (!(k === 46 ||k === 8 ||k === 9 || k >= 48 && k <= 57 || k >= 96 && k <= 105 || k >= 35 && k <= 39)) {
 			return false;
 		}
 	},
@@ -305,14 +303,14 @@ var rr = {
     addEvent: function (elem, evt, func, v) {
         var vars = rr.getVars(v, { useCapture: false } );
         elem = rr.getElement(elem);
-        if(typeof(elem) == 'object') {
+        if(typeof(elem) === 'object') {
             if (elem.addEventListener) { elem.addEventListener (evt, func, vars.useCapture); }
             else if (elem.attachEvent) { elem.attachEvent ("on" + evt, func); }
         }
     },
     
     getElement: function (sel, context) {
-        if(!sel) return false;
+        if(!sel) { return false; }
         
         if (typeof(sel) === 'object') {
             return sel;
@@ -347,7 +345,7 @@ var rr = {
     removeElement: function (elem) {
         elem = rr.getElement(elem);
 		if(elem && elem.parentNode) {
-        	elem.parentNode.removeChild(elem);
+            elem.parentNode.removeChild(elem);
 		}
     },
     
@@ -366,17 +364,17 @@ var rr = {
     showHideElement: function (elem, v) {
         var vars = rr.getVars(v, { display: 'block' } );
         elem = rr.getElement(elem);
-        elem.style.display = elem.style.display == ('none') ? (vars.display) : ('none');
+        elem.style.display = elem.style.display === ('none') ? (vars.display) : ('none');
         return false;
     },
     
     parseArray: function(str, v) { 
         var vars = rr.getVars(v, { delimiter: ';', separator: '=', mode:'array' } );
         
-        if (vars.mode == 'array') {
+        if (vars.mode === 'array') {
             var responseArray = str.split(vars.delimiter);
-        } else if (vars.mode == 'assoc') {
-            var responseArray = {};
+        } else if (vars.mode === 'assoc') {
+            var responseArray = {}, arrItem;
             var tmpArray = str.split(vars.delimiter);
             for (var i = 0; i < tmpArray.length; i++) {
                 arrItem = tmpArray[i].split(vars.separator);
@@ -406,14 +404,14 @@ var rr = {
             var vars = rr.getVars(v, { errorHandler: defaultErrorHandler, method: 'get', async: 'false' } );
             
             var makeAjaxObject = function() {
-                if      (window.XMLHttpRequest) return new XMLHttpRequest();
-                else if (window.ActiveXObject)  return new ActiveXObject("Microsoft.XMLHTTP");
+                if      (window.XMLHttpRequest) { return new XMLHttpRequest(); }
+                else if (window.ActiveXObject)  { return new ActiveXObject("Microsoft.XMLHTTP"); }
                 else    { vars.errorHandler.call(this); return false; }
             };
             
             var parseAjaxResponse = function () {
-                if(ajaxObject.readyState == 1 && vars.loadingHandler) { vars.loadingHandler.call(this, ajaxObject); }
-                if(ajaxObject.readyState == 4){ vars.responseHandler.call(this, ajaxObject); }
+                if(ajaxObject.readyState === 1 && vars.loadingHandler) { vars.loadingHandler.call(this, ajaxObject); }
+                if(ajaxObject.readyState === 4){ vars.responseHandler.call(this, ajaxObject); }
             };
             
             var ajaxObject = makeAjaxObject();
@@ -438,14 +436,14 @@ var rr = {
             var vars = rr.getVars(v);
             
             var loadingHandler = function () {
-				if (vars.loadingHandler) vars.loadingHandler.call();	
+				if (vars.loadingHandler) { vars.loadingHandler.call(); }
 			};
             
             var parseResponse = function (ajaxObject) {
                 var responseArray = rr.parseArray(ajaxObject.responseText, vars);
                 
-                if (vars.responseHandler) { vars.responseHandler.call(this, responseArray) }
-                else { return responseArray }
+                if (vars.responseHandler) { vars.responseHandler.call(this, responseArray); }
+                else { return responseArray; }
             };
             
             rr.ajax.getResponse(url, { responseHandler: parseResponse, loadingHandler: loadingHandler }); 
@@ -468,7 +466,7 @@ var rr = {
     extras: {
         installExtension: function(evt, description, icon) {
             if (InstallTrigger) {
-                var vars = new Array();
+                var vars = [];
                 vars[description] = {
                     URL      : evt.target.href,
                     IconURL  : icon,
