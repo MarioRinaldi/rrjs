f8ffc19c888c67367ac9a200d49db71bad7bb2c4
Mario Felipe Rinaldi <lalo.rinaldi@gmail.com>
2010-12-09 17:36:47 -0200

    criando css.php para juntar arquivos css

---
+++ css.php
@@ -0,0 +1,12 @@
+﻿<?php 
+
+$a = file_get_contents("rr.css");
+$b = file_get_contents("main.css");
+
+
+$a = preg_replace('/\n/','',preg_replace('/\r/','',$a));
+$b = preg_replace('/\n/','',preg_replace('/\r/','',$b));
+
+header("Content-Type: text/css; charset=utf-8");
+echo $a . $b;
+?>
