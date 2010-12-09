159521fc6ba3338a2799c882602ef8b9895d3508
Mario Felipe Rinaldi <lalo.rinaldi@gmail.com>
2010-12-09 17:44:08 -0200

    atualizando o css.php

--- css.php
+++ css.php
@@ -4,11 +4,12 @@ $a = file_get_contents("rr.css");
 $b = file_get_contents("main.css");
 
 $c = $a . $b;
+$c = trim($c);
 
 $c = preg_replace('/\n/','',$c);
 $c = preg_replace('/\r/','',$c);
 $c = preg_replace('/\t/','',$c);
 
 header("Content-Type: text/css; charset=utf-8");
-echo trim($c);
+echo $c;
 ?>
