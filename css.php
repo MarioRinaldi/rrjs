e971ee22e50708d1fea245c71a078be38e7f7ea8
Mario Felipe Rinaldi <lalo.rinaldi@gmail.com>
2010-12-09 17:56:45 -0200

    removendo espaços em branco em excesso no css

--- css.php
+++ css.php
@@ -1,15 +1,16 @@
-<?php 
+<?php
 
 $a = file_get_contents("rr.css");
 $b = file_get_contents("main.css");
 
 $c = $a . $b;
-$c = trim($c);
 
-$c = preg_replace('/\n/','',$c);
-$c = preg_replace('/\r/','',$c);
+$c = preg_replace('/\r\n/','',$c);
+$c = preg_replace('/\s{2,}/','',$c);
 $c = preg_replace('/\t/','',$c);
+$c = trim($c);
 
 header("Content-Type: text/css; charset=iso-8859-1");
 echo $c;
+
 ?>
